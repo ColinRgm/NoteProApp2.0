126eb2a804df9e6f6e6583c76ec4ba446091b99a
ColinRgm <90181373+ColinRgm@users.noreply.github.com>
2025-02-26 16:33:43 +0100

    chore: edit the page to add grade

--- resources/js/pages/addGrade.tsx
+++ resources/js/pages/addGrade.tsx
@@ -20,12 +20,12 @@ export default function AddGrade() {
             <div className="flex flex-1 flex-col items-center justify-center gap-10 rounded-xl p-4">
                 <Card className="w-[70%] p-5">
                     <CardTitle size="xxl" margin="big">
-                        Ajouter un note
+                        Ajouter une note
                     </CardTitle>
-                    <CardContent>
+                    <CardContent className="flex flex-col gap-5">
                         <Select>
-                            <SelectTrigger className="w-[180px]">
-                                <SelectValue placeholder="Choisir une branche" />
+                            <SelectTrigger className="w-[400px]">
+                                <SelectValue placeholder="Choix de la branche" />
                             </SelectTrigger>
                             <SelectContent>
                                 <SelectGroup>
@@ -38,6 +38,55 @@ export default function AddGrade() {
                                     <SelectLabel>Math / Anglais</SelectLabel>
                                     <SelectItem value="math">Math</SelectItem>
                                     <SelectItem value="anglais">Anglais</SelectItem>
+                                </SelectGroup>
+                            </SelectContent>
+                        </Select>
+
+                        <Select>
+                            <SelectTrigger className="w-[400px]">
+                                <SelectValue placeholder="Choix du semestre" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectGroup>
+                                    <SelectLabel>1ère année</SelectLabel>
+                                    <SelectItem value="semestre_1">Semestre 1</SelectItem>
+                                    <SelectItem value="semestre_2">Semestre 2</SelectItem>
+
+                                    <SelectLabel>2ème année</SelectLabel>
+                                    <SelectItem value="semestre_3">Semestre 3</SelectItem>
+                                    <SelectItem value="semestre_4">Semestre 4</SelectItem>
+
+                                    <SelectLabel>3ème année</SelectLabel>
+                                    <SelectItem value="semestre_5">Semestre 5</SelectItem>
+                                    <SelectItem value="semestre_6">Semestre 6</SelectItem>
+
+                                    <SelectLabel>4ème année</SelectLabel>
+                                    <SelectItem value="semestre_7">Semestre 7</SelectItem>
+                                    <SelectItem value="semestre_8">Semestre 8</SelectItem>
+                                </SelectGroup>
+                            </SelectContent>
+                        </Select>
+
+                        <Select>
+                            <SelectTrigger className="w-[200px]">
+                                <SelectValue placeholder="Choix de la note" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectGroup>
+                                    <SelectLabel>En dessus de la moyenne</SelectLabel>
+                                    <SelectItem value="note_6">6</SelectItem>
+                                    <SelectItem value="note_5_5">5.5</SelectItem>
+                                    <SelectItem value="note_5">5</SelectItem>
+                                    <SelectItem value="note_4_5">4.5</SelectItem>
+                                    <SelectItem value="note_4">4</SelectItem>
+
+                                    <SelectLabel>En dessous de la moyenne</SelectLabel>
+                                    <SelectItem value="note_3_5">3.5</SelectItem>
+                                    <SelectItem value="note_3">3</SelectItem>
+                                    <SelectItem value="note_2_5">2.5</SelectItem>
+                                    <SelectItem value="note_2">2</SelectItem>
+                                    <SelectItem value="note_1_5">1.5</SelectItem>
+                                    <SelectItem value="note_1">1</SelectItem>
                                 </SelectGroup>
                             </SelectContent>
                         </Select>
