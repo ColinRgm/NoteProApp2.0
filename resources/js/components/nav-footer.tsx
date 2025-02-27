50871e66c31ed0576a69142fc8b8292d9ab5c0d7
ColinRgm <90181373+ColinRgm@users.noreply.github.com>
2025-02-27 16:01:28 +0100

    fix: small bug about the new page that open the page in a new tab

--- resources/js/components/nav-footer.tsx
+++ resources/js/components/nav-footer.tsx
@@ -19,7 +19,7 @@ export function NavFooter({
                                 asChild
                                 className="text-white hover:text-neutral-800 dark:text-neutral-300 dark:hover:text-neutral-100 py-7"
                             >
-                                <a href={item.url} target="_blank" rel="noopener noreferrer">
+                                <a href={item.url} rel="noopener noreferrer">
                                     {item.icon && <Icon iconNode={item.icon} className="h-5 w-5" />}
                                     <span>{item.title}</span>
                                 </a>
