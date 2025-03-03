5abf36bf938f87724b1f90a315f2f20c4c397425
ColinRgm <colin.regamey@icloud.com>
2025-03-03 16:07:34 +0100

    fix: add the last name in the sidebar

--- resources/js/components/user-info.tsx
+++ resources/js/components/user-info.tsx
@@ -7,14 +7,16 @@ export function UserInfo({ user, showEmail = false }: { user: User; showEmail?: 
 
     return (
         <>
-            <Avatar className="h-8 w-8 overflow-hidden rounded-full ">
+            <Avatar className="h-8 w-8 overflow-hidden rounded-full">
                 <AvatarImage src={user.avatar} alt={user.first_name} />
                 <AvatarFallback className="rounded-lg bg-white text-black dark:bg-neutral-700 dark:text-white">
-                    {getInitials(user.first_name)}
+                    {getInitials(`${user.first_name} ${user.last_name}`)}
                 </AvatarFallback>
             </Avatar>
             <div className="grid flex-1 text-left text-sm leading-tight text-white">
-                <span className="truncate font-medium">{user.first_name}</span>
+                <span className="truncate font-medium">
+                    {user.first_name} {user.last_name}
+                </span>
                 {showEmail && <span className="text-muted-foreground truncate text-xs">{user.email}</span>}
             </div>
         </>
