ac4fcada3788ddd21d9819f308f6ea11396378cd
ColinRgm <colin.regamey@icloud.com>
2025-03-04 13:18:30 +0100

    fix: error with undefined value

--- resources/js/components/ui/table.tsx
+++ resources/js/components/ui/table.tsx
@@ -1,48 +1,119 @@
-interface TablesProps {
+import { useEffect, useState } from 'react';
 
-    col_one_title: string,
-    col_two_title: string,
-    col_three_title: string,
-
-    col_one_content: string,
-    col_two_content: string,
-    col_three_content: string,
+interface Grade {
+    id: number,
+    name: string,
+    pdf: string,
+    note: number,
+    branch_id: number
 }
 
-export default function Tables(
-    {
-        col_one_title,
-        col_one_content,
-        col_two_title,
-        col_two_content,
-        col_three_title,
-        col_three_content
-    }: TablesProps) {
+interface Branche {
+    id: number,
+    name: string
+}
 
-    return (
-        <>
-            {/* ----- Tableau de note ----- */}
-            <table className="w-[100%] divide-y-1 divide-gray-300">
-                {/* ----- Entête du tableau ----- */}
-                <thead className="h-20">
-                <tr>
-                    <th className="tg-0lax w-4/6 text-left text-xl">{col_one_title}</th>
-                    <th className="tg-0lax w-1/6 text-xl">{col_two_title}</th>
-                    <th className="tg-0lax w-1/6 text-xl">{col_three_title}</th>
-                </tr>
-                </thead>
 
-                {/* ----- Contenu du tableau ----- */}
-                <tbody className="divide-y divide-gray-300">
-                {/* ----- Condition d'ajout pour chaque ligne du tableau ----- */}
-                <tr className="h-15 text-center">
-                    <td className="tg-0lax text-left">{col_one_content}</td>
-                    <td className="tg-0lax">{col_two_content}</td>
-                    <td className="tg-0lax">{col_three_content}</td>
-                </tr>
-                </tbody>
-            </table>
-        </>
-    );
 
+
+export default function Tables() {
+
+    const [grades, setGrades] = useState<Grade[]>([]);
+    const [branches, setBranches] = useState<Branche[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        fetch('http://127.0.0.1:8000/api/grades')
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Erreur lors de la récupération des données...');
+                }
+                return response.json();
+            })
+            .then((data) => {
+                setGrades(data);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.error('Erreur: ', error);
+                setError(error.message);
+                setLoading(false);
+            });
+
+        fetch('http://127.0.0.1:8000/api/branches')
+            .then((response_branch) => {
+                if (!response_branch.ok) {
+                    throw new Error('Erreur lors de la récupération des données...');
+                }
+                return response_branch.json();
+            })
+            .then((data) => {
+                setBranches(data);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.error('Erreur: ', error);
+                setError(error.message);
+                setLoading(false);
+            });
+
+    }, []);
+
+    if (loading)
+        return (
+            <div className="flex flex-1 flex-col items-center justify-center gap-10 rounded-xl p-4">
+                <p>Chargement...</p>
+            </div>
+        );
+    else if (error)
+        return (
+            <div className="flex flex-1 flex-col items-center justify-center gap-10 rounded-xl p-4">
+                <p>Erreur: {error}</p>
+            </div>
+        );
+    else {
+        return (
+            <>
+                {/* ----- Tableau de note ----- */}
+                <table className="w-[100%] divide-y-1 divide-gray-300">
+                    {/* ----- Entête du tableau ----- */}
+                    <thead className="h-20">
+                    <tr>
+                        <th className="tg-0lax w-4/6 text-left text-xl">Branches / modules</th>
+                        <th className="tg-0lax w-1/6 text-xl">PDF</th>
+                        <th className="tg-0lax w-1/6 text-xl">Note</th>
+                    </tr>
+                    </thead>
+
+                    {/* ----- Contenu du tableau ----- */}
+                    <tbody className="divide-y divide-gray-300">
+                    {/* ----- Condition d'ajout pour chaque ligne du tableau ----- */}
+
+                    {
+                        grades.map(grade => {
+                            const branch = branches ? branches.find(b => b.id === grade.branch_id) : undefined;
+
+                            console.log('grade.branch_id:', grade.branch_id);
+                            console.log('branches:', branch);
+
+                            return (
+                                <tr className="h-15 text-center" key={grade.id}>
+                                    <td className="tg-0lax text-left">{branch.name}</td>
+                                    <td className="tg-0lax">
+                                        <a href={grade.pdf} target="_blank">Ouvrir</a>
+                                    </td>
+                                    <td className="tg-0lax">{grade.note}</td>
+                                </tr>
+
+                            );
+                        })
+                    }
+
+                    </tbody>
+                </table>
+            </>
+        );
+
+    }
 }
--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -4,7 +4,6 @@ import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head, Link } from '@inertiajs/react';
 import { IoAddCircleOutline } from 'react-icons/io5';
-import { useEffect, useState } from 'react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -13,125 +12,58 @@ const breadcrumbs: BreadcrumbItem[] = [
     }
 ];
 
-interface Grade {
-    id: number,
-    name: string,
-    pdf: string,
-    grade: number
-}
-
 export default function Dashboard() {
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Dashboard" />
 
-    const [grades, setGrades] = useState<Grade[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        fetch('http://127.0.0.1:8000/api/grades')
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error('Erreur lors de la récupération des données...');
-                }
-                return response.json();
-            })
-            .then((data) => {
-                setGrades(data);
-                setLoading(false);
-            })
-            .catch((error) => {
-                console.error('Erreur: ', error);
-                setError(error.message);
-                setLoading(false);
-            });
-    }, []);
-
-    if (loading)
-        return (
-            <AppLayout>
-                <div className="flex flex-1 flex-col items-center justify-center gap-10 rounded-xl p-4">
-                    <Card className="w-[70%] p-5">
-                        <p>Chargement...</p>
+            <div className="flex flex-1 flex-col items-center justify-center gap-20 rounded-xl p-4">
+                <div className="grid w-[70%] auto-rows-min gap-20 md:grid-cols-4">
+                    <Card className="p-5">
+                        <CardTitle size="lg">Culture générale</CardTitle>
+                        <CardContent className="text-4xl">5</CardContent>
                     </Card>
-                </div>
-            </AppLayout>
-        );
-    else if (error)
-        return (
-            <AppLayout>
-                <div className="flex flex-1 flex-col items-center justify-center gap-10 rounded-xl p-4">
-                    <Card className="w-[70%] p-5">
-                        <p>Erreur: {error}</p>
+
+                    <Card className="p-5">
+                        <CardTitle size="lg">Base élargie</CardTitle>
+                        <CardContent className="text-4xl">5</CardContent>
                     </Card>
-                </div>
-            </AppLayout>
-        );
-    else {
-        return (
-            <AppLayout breadcrumbs={breadcrumbs}>
-                <Head title="Dashboard" />
 
-                <div className="flex flex-1 flex-col items-center justify-center gap-20 rounded-xl p-4">
-                    <div className="grid w-[70%] auto-rows-min gap-20 md:grid-cols-4">
-                        <Card className="p-5">
-                            <CardTitle size="lg">Culture générale</CardTitle>
-                            <CardContent className="text-4xl">5</CardContent>
-                        </Card>
-
-                        <Card className="p-5">
-                            <CardTitle size="lg">Base élargie</CardTitle>
-                            <CardContent className="text-4xl">5</CardContent>
-                        </Card>
-
-                        <Card className="p-5">
-                            <CardTitle size="lg" margin="small">
-                                Informatique
-                            </CardTitle>
-                            <CardContent className="text-4xl">5</CardContent>
-                        </Card>
-
-                        <Card className="p-5">
-                            <CardTitle size="lg">Culture générale</CardTitle>
-                            <CardContent className="text-4xl">5</CardContent>
-                        </Card>
-                    </div>
-
-                    <Card className="w-[70%] p-5">
-                        <CardTitle size="xxl" margin="big">
-                            5 dernière notes
+                    <Card className="p-5">
+                        <CardTitle size="lg" margin="small">
+                            Informatique
                         </CardTitle>
-
-                        {
-                            /*
-                             * Récupérer les 5 dernières notes (en fonction de la date)
-                             * Limiter le nombre de lignes
-                             */
-                        }
-                        <CardContent>
-                            <Tables
-                                col_one_title="Branche / modules"
-                                col_two_title="PDF"
-                                col_three_title="Note"
-
-                                {
-                                    grades.map(grade => (
-                                        col_one_content = 'Module 231'
-                                    col_two_content='test.pdf'
-                                    col_three_content='5'
-                                    ))
-                                }
-                            />
-                        </CardContent>
+                        <CardContent className="text-4xl">5</CardContent>
                     </Card>
 
-                    <div>
-                        <button>
-                            <Link href="/addGrade" prefetch>
-                                <IoAddCircleOutline className="hover:[h-20 w-20] h-10 w-10" />
-                            </Link>
-                        </button>
-                    </div>
+                    <Card className="p-5">
+                        <CardTitle size="lg">Culture générale</CardTitle>
+                        <CardContent className="text-4xl">5</CardContent>
+                    </Card>
                 </div>
-            </AppLayout>
-        );
-    }
+
+                <Card className="w-[70%] p-5">
+                    <CardTitle size="xxl" margin="big">
+                        5 dernière notes
+                    </CardTitle>
+
+                    {/*
+                     * Récupérer les 5 dernières notes (en fonction de la date)
+                     * Limiter le nombre de lignes
+                     */}
+                    <CardContent>
+                        <Tables />
+                    </CardContent>
+                </Card>
+
+                <div>
+                    <button>
+                        <Link href="/addGrade" prefetch>
+                            <IoAddCircleOutline className="hover:[h-20 w-20] h-10 w-10" />
+                        </Link>
+                    </button>
+                </div>
+            </div>
+        </AppLayout>
+    );
 }
