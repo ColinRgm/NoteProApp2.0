11d3a012eac747646f1e37067f21b9c8cc40009c
ColinRgm <colin.regamey@icloud.com>
2025-03-05 14:38:38 +0100

    fix: try to fix a bug

---
+++ app/Http/Controllers/API/RoleUserController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Role;
+use Illuminate\Http\Request;
+
+class RoleUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Role::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $roles = Role::all();
+
+        return response()->json($roles);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,16 @@
 <?php
 
-use App\Http\Controllers\Database\GradesController;
+use App\Http\Controllers\API\RoleUserController;
 use App\Http\Controllers\Database\BranchController;
+use App\Http\Controllers\Database\GradesController;
 use App\Http\Controllers\Database\RoleController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/roles', [RoleController::class, 'index']);
+/*Route::get('/roles', [RoleController::class, 'getRoles']);
 
 Route::get('/branches', [BranchController::class, 'getBranches']);
 
-Route::get('/grades', [GradesController::class, 'index']);
+Route::get('/grades', [GradesController::class, 'index']);*/
 
+Route::apiResource('roles', RoleUserController::class);
